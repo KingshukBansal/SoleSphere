989ad191060b718aa58df44ad3aae0ba8b6f2cc6
KingshukBansal <bansalkingshuk@gmail.com>
2024-01-07 16:17:02 +0530

    Pushing it to github

--- solesphere/src/components/layouts/Header.js
+++ solesphere/src/components/layouts/Header.js
@@ -17,9 +17,9 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                <MdShoppingBag />
+                <MdShoppingBag size={'25px'}/>
 
-                    <Link to="/"className="navbar-brand" href="#">SHOPIT</Link >
+                    <Link to="/"className="navbar-brand" href="#">SoleSphere</Link >
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
---
+++ solesphere/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from '../components/layouts/Layout'
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="col">
+        <div className="row">
+          filter by category
+        </div>
+        <div className="row">
+          Products
+          
+        </div>
+        <div className="row"></div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
