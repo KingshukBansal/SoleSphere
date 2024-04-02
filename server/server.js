3820ae4138cb4bc5c1c4c5a626871795cfe1ae7a
KingshukBansal <bansalkingshuk@gmail.com>
2024-04-03 01:26:58 +0530

    .

--- server/server.js
+++ server/server.js
@@ -21,8 +21,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build"))); // put this line of code in app.js
-
 // Mount routers
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/category',categoryRouter);
